851319cd8bc18d40f9eb82f18e6a308ba31f6649
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-02-20 15:10:41 -0500

    Added index page, config and .htaccess

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Pair programming
+ * Team: Ekam,Laxmi, Maureen
+ * Description: Default route
+ */
+//error reporting turned on
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+
+//require vendor/autoload.php
+
+require ("vendor/autoload.php");
+
+//instantiate Fat-free
+
+$f3 = Base:: instance();
+
+//default route
+
+$f3->route('GET /', function ()
+{
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+    //echo "Pet Home";
+});
+
+//run fat free
+
+$f3-> run();
+
