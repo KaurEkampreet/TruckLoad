928c4dd70f1cbe86200f1142a7bdcf121a71d0e7
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-03-14 17:09:43 -0400

    added table for admin page

--- model/config-trucking.php
+++ model/config-trucking.php
@@ -6,3 +6,7 @@ define("DB_DSN", "mysql:dbname=mkariuki_grc");
 define("DB_USERNAME", "mkariuki_grcuser");
 define("DB_PASSWORD", '#5y@K2.gIuTz');
 
+//define("DB_DSN", "mysql:dbname=ekaurgre_grcc");
+//define("DB_USERNAME", "ekaurgre_grcuser");
+//define("DB_PASSWORD", 'SE9s)(]^gN#A');
+
